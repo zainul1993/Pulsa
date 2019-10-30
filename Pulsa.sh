4b325ca64de5cb847a59b321bfb5e4334fa79ed4
Mrcroot <50535472+reyspeed@users.noreply.github.com>
2019-10-30 16:31:00 +0700

    Add files via upload

---
+++ Pulsa.sh
@@ -0,0 +1,139 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 1
+echo "\033[32;1m=
+
+
+  __________________________
+  | $ Pulsa Gratis         |
+  | $ BY MR REY            |
+  | $ BY MR CROOT          |
+  |                        |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+
+"
+toilet -f standard -F gay "MR CROOT"
+echo "\033[31;1m=
+
+__  __ ____     ____ ____   ___   ___ _____
+|  \/  |  _ \   / ___|  _ \ / _ \ / _ \_   _|
+| |\/| | |_) | | |   | |_) | | | | | | || |
+| |  | |  _ <  | |___|  _ <| |_| | |_| || |
+|_|  |_|_| \_\  \____|_| \_\\___/ \___/ |_|
+
+"
+echo ""
+echo "By : " MR CROOT / MR REY"
+" Team : " NO TEAM"
+echo " Gmail : " reyspeed01@gmail.com"
+" Pesan : "Jangan Di Salah Gunakan BUJANK:)"
+sleep 1
+echo "ThanksTo:"
+echo "MR CROOT IS HERE"
+echo "\033[32;1m=
+  __________________________
+  | $ Pulsa Gratis         |
+  | $ BY MR REY            |
+  | $ BY MR CROOT          |
+  |                        |
+  |________________________|
+     _____||_______||____                 
+     |__________________|
+"
+echo ""
+sleep 3
+echo "HACK PULSA ALA MR CROOT HEHEHE"
+echo " Pakai +62 GOBLOK #Contoh +62xxx:"
+read p;
+
+echo "SABAR GOBLOK PROSES"
+sleep 2
+echo "\033[37;1mANDA  10%" 
+sleep 1
+echo "G  20%" 
+sleep 1
+echo "O  30%" 
+sleep 3
+echo "B  40%" 
+sleep 3
+echo "L  50%" 
+sleep 3
+echo "O  60%"
+echo "K  90%"
+sleep 5
+echo "\033[31;1mNomor Gagal Njeng...""\033[32;1mSilahkan Masukkan Kembali No Anda:"
+read p;
+sleep 3
+echo "Sabar Njeng, Proses..."
+sleep 3
+echo "\033[37;1m#10%"
+sleep 2
+echo "##20%"
+sleep 5
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 3
+echo "\033[32;1mBerapa Pulsa Yang Kau Mau Njeng:"
+read p;
+sleep 2
+echo "Sabar Njeng Proses..."
+echo "\033[37;1m#10%"
+sleep 1
+echo "##20%"
+sleep 1
+echo "###30%"
+sleep 3
+echo "####40%"
+sleep 3
+echo "#####50%"
+sleep 3
+echo "######60%"
+sleep 3
+echo "#######70%"
+sleep 4
+echo "########80%"
+sleep 3
+echo "#########90%"
+sleep 4
+echo "##########100%"
+sleep 6
+echo "\033[34;1mSukses Mamank...""\033[32;1m Tunggu 5 Menit Njenk..."
+sleep 2
+sleep 3
+echo "Ketik Exit Untuk Keluar"
+read p;
+sleep 2
+sleep 3
+sleep 4
+
+echo "Pesan:Jika Script Ini Tidak Berfungsi Pada Anda Berarti anda Goblok"
+sleep 3
+echo "Selamat Kalau Masuk Tu Pulsa Kalau GK Mampus Lu Kalau Mau Pulsa Kerja Goblok"
+sleep 2
+echo "ThanksTo Mr Croot - Mr Rey"
+sleep 2
+sleep 4
+exit
